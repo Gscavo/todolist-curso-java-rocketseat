05a70194b0959965de2211c10b6a353ffe2764ae
gscavo <gsscavo@gmail.com>
2023-10-14 13:24:41 -0300

    Fim Aula 02

--- src/main/java/br/com/guilhermecavo/todolist/user/UserModel.java
+++ src/main/java/br/com/guilhermecavo/todolist/user/UserModel.java
@@ -1,8 +1,31 @@
 package br.com.guilhermecavo.todolist.user;
 
-public class UserModel {
+import java.time.LocalDateTime;
+import java.util.UUID;
 
-    public String username;
-    public String name;
-    public String password;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    // @Column(name = "Usuário")
+    @Column(unique = true)
+    private String username;
+    // @Column(name = "Nome")
+    private String name;
+    // @Column(name = "Senha")
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
