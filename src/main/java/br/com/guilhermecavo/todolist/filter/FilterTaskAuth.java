6846978c966b9d822047b6e252a2cebef369a897
gscavo <gsscavo@gmail.com>
2023-10-14 14:28:25 -0300

    Aula 03

---
+++ src/main/java/br/com/guilhermecavo/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,63 @@
+package br.com.guilhermecavo.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.guilhermecavo.todolist.user.IUserRepository;
+import br.com.guilhermecavo.todolist.user.UserModel;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter{
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+                // Pegar a autenticação (usuário e senha)
+                String authorization = request.getHeader("Authorization");
+                // Validar usuário
+                
+                String authEncoded = authorization.substring("Basic".length()).trim();
+                
+                byte[] authDecode= Base64.getDecoder().decode(authEncoded);
+                
+                String authString = new String(authDecode);
+
+                String[] credentials = authString.split(":");
+                
+                String username = credentials[0];
+                String password = credentials[1];
+
+                UserModel user = this.userRepository.findByUsername(username);
+
+                if (this.userRepository.findByUsername(username) == null) {
+                    response.sendError(401);
+                } else {
+                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                    if (passwordVerify.verified) {
+                        filterChain.doFilter(request, response);
+                    } else {
+                        response.sendError(401);
+                    }
+                }
+                
+
+                // Validar senha
+
+                // Segue viagem
+                filterChain.doFilter(request, response);
+    }
+    
+}
